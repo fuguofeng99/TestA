e5abf9bf10d064d1fd77f021028d14a86d3090fd
fuguofeng99 <17873310652@163.com>
2020-08-08 10:55:12 +0800

    feng2_run2方法

--- src/Test1.java
+++ src/Test1.java
@@ -1,8 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Test1 {
     public static void main(String[] args) {
+        run();
         System.out.println("Hellow git!");
     }
-    public void run(){
-        System.out.println("这里是run方法1");
+    public static void run(){
+        System.out.println("这里是run方法");
+    }
+    public static  void run2(){
+        System.out.println("这里是run2方法");
     }
 }
