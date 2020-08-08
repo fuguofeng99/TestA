689911bd8554716489d3bcf2b7439877888d3f07
fuguofeng99 <17873310652@163.com>
2020-08-08 10:38:30 +0800

    feng_版本2——run方法

--- src/Test1.java
+++ src/Test1.java
@@ -2,4 +2,7 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hellow git!");
     }
+    public void run(){
+        System.out.println("这里是run方法1");
+    }
 }
